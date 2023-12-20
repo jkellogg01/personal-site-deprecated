2cdd566b40addefb58e821667392312b64d11f35
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2023-12-20 16:58:40 -0700

    right-hand display bar with profile blurb

--- client/src/components/LeftBar.tsx
+++ client/src/components/LeftBar.tsx
@@ -2,16 +2,16 @@ import Nav from "./Nav";
 
 export default function LeftBar() {
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-4 max-w-xs">
       <div className="bg-slate-900 p-4 rounded-lg text-end">
         <h1 className="text-4xl text-amber-200 font-bold">
           Joshua <wbr />
           Kellogg
         </h1>
-        <h2 className="text-xl text-amber-100 font-semibold">
+        <h3 className="text-xl text-amber-100 font-semibold">
           Full-Stack <wbr />
           Developer
-        </h2>
+        </h3>
       </div>
       <Nav />
     </div>
