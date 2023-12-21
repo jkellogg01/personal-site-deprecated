be508f9c1bdc0130a24d09d7ddb596ed59130ee1
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2023-12-21 10:54:16 -0700

    cleanup

--- src/pages/Article.tsx
+++
@@ -1,9 +0,0 @@
-export default function Article(props: { path: string }) {
-  return (
-    <embed
-      className="w-full h-full bg-slate-900 rounded-lg"
-      src={props.path}
-      type="text/markdown"
-    />
-  );
-}
--- src/pages/Home.tsx
+++
@@ -1,5 +0,0 @@
-import Article from "./Article";
-
-export default function Home() {
-  return <Article path="articles/sample-article.md" />;
-}
