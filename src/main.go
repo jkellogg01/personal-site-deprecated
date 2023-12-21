109ba29e98186e6dab50fe85d89e35d5da1e16fc
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2023-12-21 12:35:43 -0700

    server serves react app statically

--- src/main.go
+++ src/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "log"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	log.Print("yeehaw")
+	r := gin.Default()
+
+	r.Static("/dist", "./dist")
+	r.StaticFile("/", "./client/index.html")
+
+	r.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "pong!",
+		})
+	})
+
+	log.Fatal(
+		r.Run(":42069"),
+	)
 }
