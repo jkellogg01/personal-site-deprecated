e956c86d0c7a341f1af9a615fc7989ef6285e36d
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2023-12-21 14:43:48 -0700

    netlify changes to (hopefully) get esbuild to work

---
+++ client/esbuild.mjs
@@ -0,0 +1,18 @@
+import esbuild from "esbuild";
+
+console.log("Bundling...");
+esbuild
+  .build({
+    entryPoints: ["src/main.tsx"],
+    bundle: true,
+    minify: true,
+    outfile: "../dist/app.js",
+  })
+  .then((result) => {
+    console.log("Finished bundling!");
+    console.log(result);
+  })
+  .catch((err) => {
+    console.error("Bundling failed");
+    console.error(err);
+  });
