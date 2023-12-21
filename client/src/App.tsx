c354be2f8d9dec3c2f447a99614c18cb61e209ab
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2023-12-20 17:28:23 -0700

    add react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router-dom";
 import LeftBar from "./components/LeftBar";
 import RightBar from "./components/RightBar";
 
@@ -5,9 +6,7 @@ export default function App() {
   return (
     <div className="p-8 gap-4 flex flex-row justify-center items-start">
       <LeftBar />
-      <div className="bg-slate-900 p-4 rounded-xl max-w-prose">
-        This is column 2
-      </div>
+      <Outlet />
       <RightBar />
     </div>
   );
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.tsx";
+import "./index.css";
+import Home from "./pages/Home.tsx";
+import Projects from "./pages/Projects.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "projects",
+        element: <Projects />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
